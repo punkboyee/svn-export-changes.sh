962e66b13e2618c418a89e9d8336b85c8c1d2552
tianbaoqiang <tianbaoqiang@csair.com>
2014-05-29 16:49:10 +0800

    branch a deployment version for web apps

--- svn-export-changes.sh
+++ svn-export-changes.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -e
 
 if [ ! $1 ]; then
     echo "Please enter an SVN repository location"
@@ -13,20 +13,23 @@ target_directory=$4
 
 : ${revision_to:="HEAD"} 
 : ${revision_from:="PREV"}
-: ${target_directory:="export"} 
+: ${target_directory:="incre-deploy"} 
 
 
 # the grep is needed so we only get added/modified files and not the deleted ones or anything else
 # if it's a modified directory it's " M" so won't show with this command (good)
 # if it's an added directory it's still "A" so will show with this command (not so good)
 
-# clear out directory first
-rm -R $target_directory
+# clear out directory first, suppress errors
+rm -R $target_directory &2>/dev/null
 
 echo -e "\033[38;5;148mCleaning export directory\033[39m"
 
 # Begin export
 echo -e "\033[00;31mExporting files revised between $revision_from and $revision_to to /$target_directory\033[39m"
+
+# trim $repository
+repository=`echo "$repository" | sed 's|/*$||'`
 
 for line in `svn diff --summarize -r$revision_from:$revision_to $repository | grep "^[AM]"`
 do
@@ -37,12 +40,28 @@ do
     if [ $line != "A" ] && [ $line != "AM" ] && [ $line != "M" ]; then
         # use sed to remove the repository from the full repo and filename
         filename=`echo "$line" |sed "s|$repository||g"`
+        echo "$filename"
         # don't export if it's a directory we've already created
-        if [ ! -d $target_directory$filename ]; then
+        if [ ! -d $target_directory/$filename ]; then
             directory=`dirname $filename`
-            mkdir -p $target_directory$directory
-            echo "$filename"
-            svn export -qr $revision_to $line $target_directory$filename
+            echo $directory
+            IS_DIR_WEB=`echo "$directory" | grep -P '^WebContent/*'`;
+            # 1) files not under 'WebContent', exports go to 'WEB-INF/classes/'
+            # 2) files under 'WebContent', exports to the root of $target_directory
+            if [ $IS_DIR_WEB ]; then
+                # for 'src' directory, class files will be exported for java sources
+                trim_dir="WEB-INF/classes/"`echo "$directory" | sed 's|^/||' | sed 's|[^/]*/*||'`
+                mkdir -p "$target_directory/$trim_dir"
+                filename="WEB-INF/classes/"`echo "$filename" | sed 's|^/||' | sed 's|[^/]*/||' | sed 's|.java$|.class|'`
+                echo "$filename"
+                cp "$repository/WebContent/$filename" "$target_directory/$filename"
+            else
+                mkdir -p "$target_directory/$IS_DIR_WEB"
+                echo "$filename"
+                filename=`echo "$filename" | sed 's|^WebContent/||'`;
+                svn export -qr $revision_to $line $target_directory/$filename
+                exit;
+            fi
         fi
     fi
 done
